dfc0fccad25ca0c5203461a4fda161d40755fb2c
cdrectenwald <cdrectenwald@gmail.com>
2021-12-01 11:48:23 -0600

    Update day1.py

--- day1/day1.py
+++ day1/day1.py
@@ -2,17 +2,21 @@ with open('input.txt') as file:
     lines = file.readlines()
     lines = [int(line.rstrip()) for line in lines]
 
-count = 0
-count2 = 0
-for i in range(0, len(lines)):
+    
+def day1(lines):
+    count = 0
+    count2 = 0
+    for i in range(0, len(lines)):
 
-  if i >=1 and lines[i] > lines[i-1]:
-    count += 1
-  if i >= 3:
-    firstSum = lines[i-2] + lines[i-1] + lines[i-3]
-    secondSum = lines[i] + lines[i-1] + lines[i-2]
+    if i >=1 and lines[i] > lines[i-1]:
+        count += 1
+    if i >= 3:
+        firstSum = lines[i-2] + lines[i-1] + lines[i-3]
+        secondSum = lines[i] + lines[i-1] + lines[i-2]
 
-    if secondSum > firstSum:
-      count2 += 1
-print(count)
-print(count2)
+        if secondSum > firstSum:
+            count2 += 1
+    return count, count2
+
+print(day1(lines))
+
