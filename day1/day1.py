fe94620d92a098eea5c8be2ad3ae0b18caa522d3
cdrectenwald <cdrectenwald@gmail.com>
2021-12-01 00:21:27 -0600

    Create day1.py

---
+++ day1/day1.py
@@ -0,0 +1,18 @@
+with open('input.txt') as file:
+    lines = file.readlines()
+    lines = [int(line.rstrip()) for line in lines]
+
+count = 0
+count2 = 0
+for i in range(0, len(lines)):
+
+  if i >=1 and lines[i] > lines[i-1]:
+    count += 1
+  if i >= 3:
+    firstSum = lines[i-2] + lines[i-1] + lines[i-3]
+    secondSum = lines[i] + lines[i-1] + lines[i-2]
+
+    if secondSum > firstSum:
+      count2 += 1
+print(count)
+print(count2)
