97b756706c75699d7821f9400ed9ab9559547473
cdrectenwald <cdrectenwald@gmail.com>
2021-12-15 22:15:32 -0600

    Update day14.py

--- day14/day14.py
+++ day14/day14.py
@@ -42,37 +42,4 @@ while steps < 40:
 
 
 print(origString)
-from collections import Counter
-freqCounter =  Counter(origString)
-print(max(freqCounter.values())-min(freqCounter.values()))
 
-
-
-
-S, rules = open("input.txt").read().split('\n\n')
-
-R = {}
-for line in rules.strip().split('\n'):
-    start,end = line.strip().split(' -> ')
-    R[start] = end
-
-# Keep track of *counts* of each pair of letters
-# C1 = {pair of characters: count of that pair}
-C1 = Counter()
-for i in range(len(S)-1):
-    C1[S[i]+S[i+1]] += 1
-
-for t in range(41):
-    if t in [10,40]:
-        CF = Counter()
-        for k in C1:
-            CF[k[0]] += C1[k]
-        CF[S[-1]] += 1
-        print(max(CF.values())-min(CF.values()))
-
-    # If AB->R, then AB becomes (AR, RB)
-    C2 = Counter()
-    for k in C1:
-        C2[k[0]+R[k]] += C1[k]
-        C2[R[k]+k[1]] += C1[k]
-    C1 = C2
